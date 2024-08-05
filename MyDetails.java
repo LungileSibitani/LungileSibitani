8f41497806f2c45d16b735b94290a200865613f5
LungileSibitani <167187931+LungileSibitani@users.noreply.github.com>
2024-08-05 21:03:26 +0200

    Add files via upload
    My names

---
+++ MyDetails.java
@@ -0,0 +1,24 @@
+
+package lungile.sibitani;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+
+public class MyDetails  extends  JFrame{
+
+    public MyDetails() {
+        setTitle("Lungile Sibitani");
+        setSize(300, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(true);
+        setDefaultLookAndFeelDecorated(true);
+        setForeground(Color.red);
+        setVisible(true);
+        
+        
+    }
+    
+    
+    
+}
---
+++ NameAndSurname.java
@@ -0,0 +1,15 @@
+
+package nameandsurname;
+
+import lungile.sibitani.MyDetails;
+
+
+public class NameAndSurname {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+      MyDetails details = new MyDetails();
+    }
+    
+}
